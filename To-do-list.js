324f36ba22b58882f33d30f26d4c3b2e6f29b090
Aravind-KP-Rajan <aravind777kp@gmail.com>
2024-06-04 13:30:41 +0530

    Used accumulator pattern in for loop & displayed the array values in the webpage

--- To-do-list.js
+++ To-do-list.js
@@ -1,4 +1,29 @@
-const toDoList = [];
+/* Main Idea of JavaScript
+1. Save the Data
+2.Generate the HTML
+3.Make itt interactive
+*/
+
+const toDoList = ['make atho', 'read books']; //1. save the data
+
+
+renderTodoList();
+
+function renderTodoList(){
+  
+  let toDoListHTML =''; //accumulating variable
+  
+  for(i=0;i<toDoList.length;i++){
+    const todo = toDoList[i];
+    //2.use the data to generate HTML
+    const html = `<p>${todo}</p>`; //generating the HTML
+    toDoListHTML+=html; //accumulating the value into the variable
+  }
+  
+  console.log(toDoListHTML);
+  
+  document.querySelector('.js-todo-list').innerHTML = toDoListHTML;
+}
 
 function addTool(){
 
@@ -12,4 +37,6 @@ function addTool(){
   console.log(toDoList);
 
   inputElement.value='';
+
+  renderTodoList();
 }
